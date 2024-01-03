b98f9fa806c37c8a1b04b14a74b5c369da16c9e5
sisco001 <sisco001@Ubuntu.myguest.virtualbox.org>
2024-01-04 00:51:45 +0100

    done

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,5 +1,5 @@
 export default function getResponseFromAPI() {
-    const success = true
+    const success = true;
     return new Promise((resolve, reject) => {
         if (success) {
             resolve();
