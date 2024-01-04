a8f39598f3b7a1e991c6b6012615c28c4d783e00
OmarTl2005 <hiba41145@gmail.com>
2024-01-04 01:48:10 +0100

    done

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,11 @@
-import { uploadPhoto, createUser } from './utils';
+import { createUser, uploadPhoto } from './utils';
 
-export default function handleProfileSignup() {
+function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then((data) => { console.log(`${data[0].body} ${data[1].firstName} ${data[2].lastName}`); })
+    .then((data) => {
+      console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`);
+    })
     .catch(() => console.log('Signup system offline'));
 }
+
+export default handleProfileSignup;
