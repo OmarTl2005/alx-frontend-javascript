0024dafd15fb37d47da34f6d8cabaa93aa284014
OmarTl2005 <hiba41145@gmail.com>
2024-01-08 19:12:56 +0100

    done

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,19 +1,19 @@
 export default function updateStudentGradeByCity(list, city, newGrades) {
-    const newList = list.filter((student) => student.location === city);
+  const newList = list.filter((student) => student.location === city);
 
-    for (const student of newList) {
-        student.grade = 'N/A';
+  for (const student of newList) {
+    student.grade = 'N/A';
+  }
+
+  const newStudent = newList.map((student) => {
+    const temp = student;
+    for (const grade of newGrades) {
+      if (temp.id === grade.studentId) {
+        temp.grade = grade.grade;
+      }
     }
+    return temp;
+  });
 
-    const newStudent = newList.map((student) => {
-        const temp = student;
-        for (const grade of newGrades) {
-            if (temp.id === grade.studentId) {
-                temp.grade = grade.grade;
-            }
-        }
-        return temp;
-    })
-
-    return newStudent;
+  return newStudent;
 }
