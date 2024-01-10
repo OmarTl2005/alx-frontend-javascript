5216a57fc8b101faf94843c674794d98084aef0c
OmarTl2005 <hiba41145@gmail.com>
2024-01-11 00:09:08 +0100

    done

--- 0x04-TypeScript/task_0/js/main.js
+++ 0x04-TypeScript/task_0/js/main.js
@@ -1,27 +1,32 @@
 var student1 = {
-    firstName: 'John',
-    lastName: 'Doe',
-    age: 25,
-    location: 'San Francisco'
+    firstName: "Zeliq",
+    lastName: "Shannon",
+    age: 40,
+    location: "Nakuru"
 };
 var student2 = {
-    firstName: 'James',
-    lastName: 'leBron',
-    age: 39,
-    location: 'Los Angeles'
+    firstName: "John",
+    lastName: "Doe",
+    age: 37,
+    location: "Nairobi"
 };
-var student = [student1, student2];
+var studentsList = [student1, student2];
 var table = document.createElement('table');
 var tbody = document.createElement('tbody');
+table.style.background = "pink";
 table.appendChild(tbody);
-student.forEach(function (student) {
+studentsList.forEach(function (student) {
     var row = document.createElement('tr');
-    var name = document.createElement('td');
-    var locations = document.createElement('td');
-    name.textContent = student.firstName;
-    locations.textContent = student.location;
-    row.appendChild(name);
-    row.appendChild(locations);
+    var nameCell = document.createElement('td');
+    var locationCell = document.createElement('td');
+    nameCell.textContent = student.firstName;
+    locationCell.textContent = student.location;
+    nameCell.style.border = "1px solid pink";
+    locationCell.style.border = "1px solid pink";
+    nameCell.style.padding = "5px";
+    locationCell.style.padding = "5px";
+    row.appendChild(nameCell);
+    row.appendChild(locationCell);
     tbody.appendChild(row);
 });
 document.body.appendChild(table);
